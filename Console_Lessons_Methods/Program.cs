006c93e3a006982eda69558804c09e3bbfd9f33d
TannorWallace <tannor.wallace@gmail.com>
2020-01-03 14:01:16 -0700

    Committing new lessons

---
+++ Console_Lessons_Methods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console_Lessons_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
